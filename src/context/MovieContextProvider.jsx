b094ca5068129b963872402cefa25ffe1ba43d24
Tolga <06thy40@gmail.com>
2022-07-17 11:23:27 +0300

    continues

--- src/context/MovieContextProvider.jsx
+++ src/context/MovieContextProvider.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext } from "react";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const MovieContext = createContext();
 
@@ -8,20 +9,27 @@ export const useMovieContext = () => {
 };
 
 const MovieContextProvider = ({ children }) => {
-  const [movies, setMovies] = useState([]);
+  const [detailMovies, setDetailMovies] = useState([]);
   const [showBtn, setShowBtn] = useState(false);
 
-  useEffect(() => {
-    const movie = movies.map((movie) => movie);
+  //  details
+  const getMovies = async (id) => {
+    try {
+      const res = await axios(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=12703b02c36476b0b7413bc8dc2a926e`)
+      const data = res.data;
+      setDetailMovies(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    fetch(
-      `https://api.themoviedb.org/3/movie/${movie.id}?api_key=12703b02c36476b0b7413bc8dc2a926e`
-    )
-      .then((res) => res.json())
-      .then((data) => setMovies(data));
-  }, []);
+  fetch(
+    `https://api.themoviedb.org/3/movie/${movie.id}?api_key=12703b02c36476b0b7413bc8dc2a926e`
+  )
+    .then((res) => res.json())
+    .then((data) => setDetailMovies(data));
 
-  const values = { movies, showBtn, setShowBtn };
+  const values = { showBtn, setShowBtn };
 
   return (
     <MovieContext.Provider value={values}>{children}</MovieContext.Provider>
